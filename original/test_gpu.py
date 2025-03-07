e696a4c006b97b351b35626e1dd43d3e63ce848b
Tom <tom.uijtdehaag@bigdatarepublic.nl>
2025-03-07 13:06:17 +0100

    Initial commit - initialise uv package

--- original/test_gpu.py
+++ original/test_gpu.py
@@ -1,20 +1,33 @@
-import ee
-import torch
 import os
-import pandas as pd
-import geopandas as gpd
+
 import cudf
 import cuspatial
-from tqdm import tqdm
-from Burn_Scar_Detection_Utils import validate_FIRMS
+import ee
+import geopandas as gpd
+import pandas as pd
 from shapely.geometry import Point
+from tqdm import tqdm
+
+from arson_analyser.Burn_Scar_Detection_Utils import validate_FIRMS
 
 # Use absolute paths
 BASE_DIR = r"C:\Tarig\work\CIR\Sudan Witness\FIRMS Automation WorkSpace\Arson_Analyser"
-FIRMS_DATA_PATH = os.path.join(BASE_DIR, "Data", "FIRMS_Detections_2024_05_01_to_2024_05_19.geojson")
-URBAN_FILTER_PATH = os.path.join(BASE_DIR, "Data", "Sudan_Filterer_Cleaned_Dissolved.gpkg")
-OUTPATH_POLYGONS = os.path.join(BASE_DIR, "Outputs", "Validated_FIRMS_Detections_Town_Polygons_2024_04_01_to_2024_04_15.geojson")
-OUTPATH_POINTS = os.path.join(BASE_DIR, "Outputs", "Validated_FIRMS_Detections_Points_2024_04_01_to_2024_04_15.geojson")
+FIRMS_DATA_PATH = os.path.join(
+    BASE_DIR, "Data", "FIRMS_Detections_2024_05_01_to_2024_05_19.geojson"
+)
+URBAN_FILTER_PATH = os.path.join(
+    BASE_DIR, "Data", "Sudan_Filterer_Cleaned_Dissolved.gpkg"
+)
+OUTPATH_POLYGONS = os.path.join(
+    BASE_DIR,
+    "Outputs",
+    "Validated_FIRMS_Detections_Town_Polygons_2024_04_01_to_2024_04_15.geojson",
+)
+OUTPATH_POINTS = os.path.join(
+    BASE_DIR,
+    "Outputs",
+    "Validated_FIRMS_Detections_Points_2024_04_01_to_2024_04_15.geojson",
+)
 
 print("Loading FIRMS data...")
 firms_data = gpd.read_file(FIRMS_DATA_PATH)
@@ -31,7 +44,9 @@ filterer_polygons = cuspatial.GeoSeries.from_geopandas(filterer.geometry)
 
 print("Performing Urban Filtering using GPU... this may take up to 20 minutes.")
 urban_firms_detections = cuspatial.point_in_polygon(firms_points, filterer_polygons)
-print(f'A total of {urban_firms_detections.sum()} fires detected by FIRMS in urban areas.')
+print(
+    f"A total of {urban_firms_detections.sum()} fires detected by FIRMS in urban areas."
+)
 
 # Convert the results back to a GeoPandas DataFrame
 urban_firms_detections_gdf = firms_data[urban_firms_detections.to_pandas()]
@@ -43,8 +58,8 @@ print("Associating urban areas with FIRMS detections...")
 for i in tqdm(range(len(urban_firms_detections_gdf))):
     point_geom = urban_firms_detections_gdf.geometry.iloc[i]
     for index, row in filterer_polygons_exploded.iterrows():
-        if row['geometry'].intersects(point_geom):
-            intersecting_polygon = row['geometry']
+        if row["geometry"].intersects(point_geom):
+            intersecting_polygon = row["geometry"]
             urban_firms_detections_gdf.loc[i, "geometry"] = intersecting_polygon
             break
 
@@ -53,22 +68,29 @@ ee.Initialize(project="arson-analyser-423823")
 
 print("Analysing each urban FIRMS detection for evidence of arson...")
 
+
 def arson_analyser(urban_firms_detections_gdf):
     firms_detections = urban_firms_detections_gdf
     # Ensure columns exist and are of correct type
-    if 'too_cloudy' not in firms_detections.columns:
-        firms_detections['too_cloudy'] = pd.Series(dtype='bool')
-    if 'burn_scar_detected' not in firms_detections.columns:
-        firms_detections['burn_scar_detected'] = pd.Series(dtype='bool')
+    if "too_cloudy" not in firms_detections.columns:
+        firms_detections["too_cloudy"] = pd.Series(dtype="bool")
+    if "burn_scar_detected" not in firms_detections.columns:
+        firms_detections["burn_scar_detected"] = pd.Series(dtype="bool")
 
     for i in tqdm(range(len(firms_detections))):
         data_dict = validate_FIRMS(firms_detections, i)
-        firms_detections.loc[i, 'too_cloudy'] = data_dict['too_cloudy']
-        if not data_dict['too_cloudy']:
-            firms_detections.loc[i, 'burn_scar_detected'] = data_dict['burn_scar_detected']
-            if data_dict['burn_scar_detected']:
-                firms_detections.loc[i, 'burnt_pixel_count'] = data_dict['burnt_pixel_count']
-                firms_detections.loc[i, 'num_burnt_buildings'] = data_dict['num_burnt_buildings']
+        firms_detections.loc[i, "too_cloudy"] = data_dict["too_cloudy"]
+        if not data_dict["too_cloudy"]:
+            firms_detections.loc[i, "burn_scar_detected"] = data_dict[
+                "burn_scar_detected"
+            ]
+            if data_dict["burn_scar_detected"]:
+                firms_detections.loc[i, "burnt_pixel_count"] = data_dict[
+                    "burnt_pixel_count"
+                ]
+                firms_detections.loc[i, "num_burnt_buildings"] = data_dict[
+                    "num_burnt_buildings"
+                ]
     firms_detections_points = firms_detections.copy()
     for i in range(len(firms_detections_points)):
         lat = firms_detections_points.loc[i, "latitude"]
@@ -77,11 +99,14 @@ def arson_analyser(urban_firms_detections_gdf):
         firms_detections_points.loc[i, "geometry"] = point_geom
     return firms_detections, firms_detections_points
 
-validated_firms_polygons, validated_firms_detections_points = arson_analyser(urban_firms_detections_gdf)
+
+validated_firms_polygons, validated_firms_detections_points = arson_analyser(
+    urban_firms_detections_gdf
+)
 
 
 print("Performing spatial join...")
-result_gdf = gpd.sjoin(selected_gdf, join_gdf, how='left', predicate='intersects')
+result_gdf = gpd.sjoin(selected_gdf, join_gdf, how="left", predicate="intersects")
 
 
 validated_firms_polygons.to_file(OUTPATH_POLYGONS)
