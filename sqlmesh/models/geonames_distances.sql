cf2958900de860c59d5ea28076b1e09cea724904
Tom Uijtdehaag <t.j.l.uijtdehaag@gmail.com>
2025-05-22 22:28:39 +0200

    Blueprint nearest_geonames

---
+++ sqlmesh/models/geonames_distances.sql
@@ -0,0 +1,36 @@
+MODEL (
+  name arson.nearest_geonames_@source_table,
+  description 'Find the nearest geoname settlement for each FIRMS event.',
+  kind view,
+  blueprints (
+    (source_table := firms_validated_clustered, id_col := area_include_id),
+    (source_table := firms_validated, id_col := firms_id),
+  ),
+  audits (
+    NUMBER_OF_ROWS(threshold := 1)
+  ),
+);
+WITH distances AS (
+  SELECT
+    s.@id_col,
+    g.name AS settlement_name,
+    ST_DISTANCE(s.geom, g.geom) AS distance
+  FROM arson.@source_table AS s
+  LEFT JOIN arson.geonames AS g
+    ON ST_DWithin(
+      s.geom,
+      g.geom,
+      300000  -- limit to 30km
+    )
+), ranked AS (
+  SELECT
+    *,
+    ROW_NUMBER() OVER (PARTITION BY @id_col ORDER BY distance ASC) AS rn
+  FROM distances
+)
+SELECT
+  @id_col,
+  settlement_name
+FROM ranked
+WHERE
+  rn = 1
