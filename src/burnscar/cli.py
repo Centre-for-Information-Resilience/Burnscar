37d5fa8b178ce2d6767630305a3b6b80122aea00
Tom Uijtdehaag <t.j.l.uijtdehaag@gmail.com>
2025-07-07 16:55:45 +0200

    Add export command to cli to write data to disk

--- src/burnscar/cli.py
+++ src/burnscar/cli.py
@@ -4,6 +4,8 @@ from pathlib import Path
 
 import typer
 
+from sqlmesh.core.context import Context
+
 app = typer.Typer(name="burnscar", help="CLI for Burnscar, a SQLMesh project.")
 
 
@@ -34,6 +36,39 @@ def run():
     """
     ensure_sqlmesh_root()
     subprocess.run(["sqlmesh", "run"], check=True)
+
+
+@app.command()
+def export(path: Path = typer.Option(None)) -> None:
+    ensure_sqlmesh_root()
+    context = Context(paths=["."])
+    engine = context.engine_adapter
+
+    if not path:
+        path = context.config.variables.get("path_output")
+
+    try:
+        with engine.connection as conn:
+            table = context.resolve_table("mart.firms_validated")
+            conn.execute(f"""
+                            COPY {table}
+                            TO '{path}/output.csv' WITH (
+                            HEADER,
+                            DELIMITER ',')
+                        """)
+
+            table = context.resolve_table("mart.firms_validated_clustered")
+            conn.execute(f"""
+                            COPY {table}
+                            TO '{path}/output_clustered.csv' WITH (
+                            HEADER,
+                            DELIMITER ',')
+                        """)
+            typer.secho(f"Successfully stored outputs at {path}", fg="green")
+
+    except Exception as e:
+        typer.secho(f"Failed writing outputs: {e}", err=True, fg="red")
+        sys.exit(1)
 
 
 if __name__ == "__main__":
